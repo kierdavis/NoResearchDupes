12ec4dcc7dcf979705e40bf9505922337be7b08e
Kier Davis <me@kierdavis.com>
2021-03-09 23:18:47 +0000

    Update release script
    * Verify that AddOnVersion matches the git tag.
    * Print the path of the output file.

--- tools/release.sh
+++ tools/release.sh
@@ -10,14 +10,23 @@ fi
 name=NoResearchDupes
 version="$1"
 root="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
+output="$root/release/$name-$version.zip"
+
+# Sanity check.
+if ! git show "v$version:$name.txt" | grep -qE "AddOnVersion: $version\b"; then
+  echo >&2 "$name.txt AddOnVersion is wrong, refusing to release."
+  exit 1
+fi
 
 cd "$root"
 mkdir -p "$root/release"
 git archive \
   --format=zip \
   --prefix="$name/" \
-  --output="$root/release/$name-$version.zip" \
+  --output="$output" \
   "v$version" \
   "$name.txt" \
   "LICENSE.txt" \
   $(find . -name '*.lua')
+
+echo >&2 "Wrote $output"
